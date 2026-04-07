53cd58e32924303800a8660dafb4277f6c200b67
Richard <271030543+richard7cv@users.noreply.github.com>
2026-04-07 16:53:34 +0100

    Remove hello-world text

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,3 @@
 // scripts/main.js
 
-const greetingElement = document.getElementById('greeting-text');
 
-// Set the initial text
-greetingElement.textContent = 'Hello World';
-
-// Add the click listener
-greetingElement.addEventListener('click', () => {
-    if (greetingElement.textContent === 'Hello World') {
-        greetingElement.textContent = 'You clicked me!';
-    } else {
-        greetingElement.textContent = 'Hello World';
-    }
-});
